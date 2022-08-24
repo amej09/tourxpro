dfaf3b82e2acc9d86671331ae063e0b999e55fd9
ahmed <fl.ahmedbadia@gmail.com>
2022-08-24 15:06:40 +0100

    Buggy Activity

---
+++ english/activity-buggy.php
@@ -0,0 +1,320 @@
+<!doctype html>
+<html lang="en">
+
+
+    <?php include('template/header.php')?>
+
+
+
+    <div class="breadcrumb breadcrumb-style-one">
+        <div class="container">
+            <div class="col-lg-12 text-center">
+                <h2 class="breadcrumb-title">Marrakech Desert Buggy Tour :  </h2>
+                <ul class="d-flex justify-content-center breadcrumb-items">
+                    <li class="breadcrumb-item"><a href="index.php">acceuil</a></li>
+                    <li class="breadcrumb-item active">Activity</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="package-details-wrapper pt-76">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-4">
+                    <div class="package-sidebar">
+                        <aside class="package-widget-style-2 widget-form mt-30">
+                            <div class="widget-title">
+                                <table class="table" style="color: white; border: solid 0px ;border-color: #162b32; ">
+                                    <tr  style="height: 40px ;text-align: center;">
+                                        <th   >Privat</th>
+                                    </tr>
+                                    <tr  style="height: 40px ;text-align: center;">
+                                        <th   >67 €</th>
+                                    </tr>
+                                    <tr  style="height: 40px;text-align: center; ">
+                                        <th > Per person </th>                                        
+                                    </tr>
+                                   
+                                </table>
+                            </div>
+
+                            <div class="widget-body">
+                                <form action="../sender.php" method="post" id="booking-form">
+                                    <div class="booking-form-wrapper">
+                                        <div class="custom-input-group">
+                                            <input type="hidden"      name="type_reservation"  value="prive"/> &nbsp;
+                                            <input type="hidden"      name="type_tour" value="Activity"/> &nbsp;
+                                            <input type="hidden"      name="name_tour" value="Buggy"/> &nbsp;
+
+                                            Select date and participants: 
+                                            <i class="bi bi-calendar3"></i>
+                                            <input placeholder="Select your date" type="date" name="date"
+                                                id="datepickerprive" value="" class="calendar" required>
+                                        </div>
+                                        <div class="row">
+                                            <div class="custom-input-group">
+                                                <i class="bi bi-chevron-down"></i>
+                                                <select id="truist-adult" name="numbertravels">
+                                                    <option  value="notselected" selected > Number of travelers: </option>
+                                                    <option value="1"> 1</option>
+                                                    <option value="2"> 2</option>
+                                                    <option value="3"> 3</option>
+                                                    <option value="4"> 4</option>
+                                                    <option value="5"> 5</option>
+                                                </select>
+                                            </div>
+                                        </div><br>
+                                         
+                                        <div class="custom-input-group">
+                                       
+                                            Total Price in  €<input type="text"  id="totalpriceprive"  name="totalprice" class="form-control" disabled>
+                                        </div>
+                                        <div class="custom-input-group">
+                                            <input type="text" placeholder="Your Full Name" id="name" name="name" required>
+                                        </div>
+                                        <div class="custom-input-group">
+                                            <input type="email" placeholder="Your Email" id="email" name="email" >
+                                        </div>
+                                        <div class="custom-input-group">
+                                            <input type="tel" placeholder="Phone" id="phone" name="phone" required>
+                                        </div>
+                                   
+                                       
+                                       
+                                        <div class="custom-input-group">
+                                            <textarea cols="20" rows="7" name="message" placeholder="Your message"></textarea>
+                                        </div>
+                                        <div class="custom-input-group">
+                                            <div class="submite-btn">
+                                                <button type="submit" >Book Now</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </aside>
+                    </div>    
+                 </div>
+                 <div class="col-lg-8">
+                    <div class="tour-package-details">
+                        <div class="pd-header">
+                            <div class=" pd-top row row-cols-lg-4 row-cols-md-2 row-cols-2 gy-4">
+                                <div class="col">
+                                    <div class="pd-single-info">
+                                        <div class="info-icon">
+                                            <img src="../assets/images/icons/pd1.svg" alt="">
+                                        </div>
+                                        <div class="info">
+                                            <h6>Duration</h6>
+                                            <span>4 Hours</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="col">
+                                    <div class="pd-single-info">
+                                        <div class="info-icon">
+                                            <img src="../assets/images/icons/pd2.svg" alt="">
+                                        </div>
+                                        <div class="info">
+                                            <h6>Departure time</h6>
+                                            <span> At your choice</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="pd-single-info">
+                                        <div class="info-icon">
+                                            <img src="../assets/images/icons/pd4.svg" alt="">
+                                        </div>
+                                        <div class="info">
+                                            <h6> Availability </h6>
+                                            <span>Every day</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pd-thumb">
+                                <img src="../assets/images/Activite/buggy1.jpg" alt="">
+                            </div>
+                            <div class="header-bottom">
+                                <div class="pd-lavel d-flex justify-content-between align-items-center flex-wrap gap-2">
+                                    <h5 class="location"><i class="bi bi-geo-alt"></i> marrakech, morocco</h5>
+                                    <ul class="d-flex align-items-center rating">
+                                        <li><i class="bi bi-star-fill"></i></li>
+                                        <li><i class="bi bi-star-fill"></i></li>
+                                        <li><i class="bi bi-star-fill"></i></li>
+                                        <li><i class="bi bi-star-fill"></i></li>
+                                        <li><i class="bi bi-star-fill"></i></li>
+                                    </ul>
+                                </div>
+                                <h2 class="pd-title">Marrakech Desert Buggy Tour Including Berber Tea Break and transfer</h2>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+               
+
+            </div>
+            <br>
+            <div class="row ">
+                
+                <div class="col-lg-12">
+                    <div class="tour-package-details">
+
+                        <div class="package-details-tabs">
+                                <ul class="nav nav-pills tab-switchers gap-xxl-4 gap-3" id="pills-tab" role="tablist">
+                                    <li class="nav-item navmargin" role="presentation">
+                                        <button class="nav-link active" id="pills-package1" data-bs-toggle="pill"
+                                            data-bs-target="#pill-body1" type="button" role="tab" aria-controls="pill-body1"
+                                            aria-selected="true"><i class="bi bi-info-lg"></i> Information</button>
+                                    </li>
+                                    
+                                    <li class="nav-item navmargin" role="presentation">
+                                        <button class="nav-link" id="pills-package3" data-bs-toggle="pill"
+                                            data-bs-target="#pill-body3" type="button" role="tab" aria-controls="pill-body3"
+                                            aria-selected="false"><i class="bi bi-images"></i> Tour Gallary</button>
+                                    </li>
+                                    
+                                  
+                                </ul>
+                            <div class="tab-content" id="pills-tabContent">
+
+                                <div class="tab-pane fade show active package-info-tab mt-3" id="pill-body1"
+                                    role="tabpanel" aria-labelledby="pills-package1">
+                                    <h3 class="d-subtitle">Package Details</h3>
+ <pre>
+
+
+ Go off the beaten track and explore Morocco’s rugged desert landscapes by all-terrain quad bike on this adventure excursion. Swap the 
+ hustle and bustle of city for the solitude and beauty of the desert, motoring with your guide along dusty trails under the shadow of 
+ soaring mountain peaks. Eenjoy vistas of arid riverbeds, natural palm groves, and primordial volcanic terrain, plus take a break for M
+ oroccan tea and snack with a local family in a Berber village.
+
+Tour Duration: 4 hours
+Availability: Every day
+
+    <i class='bx bx-check'></i>  Quad bike tour in the desert: Escape the city hustle and bustle
+    <i class='bx bx-check'></i>  Explore desert valleys and traditional Berber villages
+    <i class='bx bx-check'></i> Hassle-free pickup and drop-off from your Marrakech hotel
+    <i class='bx bx-check'></i> Enjoy a pot of Moroccan tea and a snack with a local family
+
+What is included?
+
+    <i class='bx bx-check'></i> Hotel pickup and drop-off
+    <i class='bx bx-check'></i> Professional guide
+    <i class='bx bx-check'></i> Safety briefing
+    <i class='bx bx-check'></i> Use of buggy, helmet, gloves and goggles
+    <i class='bx bx-check'></i> Bottled water
+    <i class='bx bx-check'></i> Tea and snack
+
+What is NOT included? 
+    <i class='bx bx-check'></i> Gratuities
+    <i class='bx bx-check'></i> Food and drinks, unless specified
+
+    
+
+Meeting place:
+
+    <i class='bx bx-check'></i> Our driver will pick you up at your hotel/Riad or any other location at the agreed time.
+    <i class='bx bx-check'></i> In case of any problem: please email us or call us on the numbers displayed on contact informations.
+
+Easy Booking: Accepted until 6pm the day before (Marrakech time), If you have any difficulties to book or need another service, please use our Contact Form
+
+ 
+Payment Methods:
+
+    In cash (Euros or MAD) upon arrival
+ </pre>                    
+                                </div>
+                                <div class="tab-pane fade package-gallary-tab mt-3" id="pill-body3" role="tabpanel"
+                                    aria-labelledby="pills-package3">
+                                    <div class="row g-4">
+                                        <div class="col-6">
+                                            <div class="package-gallary-item">
+                                                <img  src="../assets/images/Activite/buggy1.jpg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="package-gallary-item">
+                                                <img  src="../assets/images/Activite/buggy2.jpg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                                <div class="package-gallary-item">
+                                                    <img  src="../assets/images/Activite/buggy3.jpg" alt="">
+                                                </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="package-gallary-item">
+                                                <img  src="../assets/images/Activite/buggy4.jpg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                                <div class="package-gallary-item">
+                                                    <img  src="../assets/images/Activite/buggy5.jpg" alt="">
+                                                </div>
+                                        </div>
+                                        <div class="col-6">
+                                                <div class="package-gallary-item">
+                                                    <img  src="../assets/images/Activite/buggy6.jpg" alt="">
+                                                </div>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                </div>
+                                
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+    </div>
+
+    <?php include('template/footer.php')?>
+
+
+ 
+    <script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="../assets/js/jquery-3.6.0.min.js"></script>
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/chain_fade.js"></script>
+    <script src="../assets/js/owl.carousel.min.js"></script>
+    <script src="../assets/js/swiper-bundle.min.js"></script>
+    <script src="../assets/js/jquery.fancybox.min.js"></script>
+    <script src="../assets/js/select2.min.js"></script>
+    <script src="../assets/js/jquery-ui.js"></script>
+
+    <script src="../assets/js/main.js"></script>
+    
+    <script >
+        var nbr_prive=67
+        $('#totalpriceprive').val(nbr_prive );
+
+       
+
+       $( '#truist-adult' ).change(function() {
+      
+        nbr= $( '#truist-adult' ).val();
+        nbr_prive=67;
+         if(nbr=='3'){
+          nbr_prive+=1;
+       }else if(nbr=='4'){
+          nbr_prive+=2;
+       }
+       $('#totalpriceprive').val(nbr_prive );
+
+       });
+</script>
+
+</body>
+
+<!-- Mirrored from demo.egenslab.com/html/tourxpro/demo/package-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 18 Aug 2022 11:46:30 GMT -->
+
+</html>
